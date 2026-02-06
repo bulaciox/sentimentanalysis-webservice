8fa0a2930971f62b625e0f21f87eeb62f9e36458
bulaciox <francobatillas@gmail.com>
2026-02-06 10:47:50 +0100

    dummy analysis

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from sentimentanalysis-webservice!")
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/v1/sentiment")
+def analyze_sentiment(text: TextInput):
+    lowered_text = text.text.lower()
+
+    if 'god' in lowered_text or 'good' in lowered_text:
+        return {"score": 3}
+    elif 'dårlig' in lowered_text or 'bad' in lowered_text:
+        return {"score": -3}
+    else:
+        return {"score": 0}
