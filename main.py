d47308faf947125e557c8f5eac63a100bc627391
bulaciox <francobatillas@gmail.com>
2026-02-26 17:11:21 +0100

    spacy v3 sentiment analysis

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
+
 from afinn import Afinn
+
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
 
 app = FastAPI()
 afinn = Afinn()
+
+# initialize spacy and add the spacytextblob component for english sentiment analysis
+nlp_en = spacy.load("en_core_web_sm")
+
+nlp_en.add_pipe('spacytextblob')
 
 @app.get("/")
 def read_root():
@@ -31,4 +40,11 @@ def analyze_sentiment(text: TextInput):
 @app.post("/v2/sentiment")
 def analyze_sentiment_v2(text: TextInput):
     return {"score": afinn.score(text.text)}
+
+@app.post("/v3/sentiment")
+def analyze_sentiment_v3(text: TextInput):
+    doc = nlp_en(text.text)
+    sentiment = doc._.blob.polarity
+    return {"score": sentiment}
+
     
